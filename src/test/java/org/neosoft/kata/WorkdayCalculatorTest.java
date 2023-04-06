e17f9b9e7902bea1f5bc96cca801a6016b27f219
Sofiane Abdallah <soufatn@msn.com>
2023-04-06 12:14:00 +0200

    add failed test

---
+++ src/test/java/org/neosoft/kata/WorkdayCalculatorTest.java
@@ -0,0 +1,13 @@
+package org.neosoft.kata;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WorkdayCalculatorTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
